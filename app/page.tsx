c80410ceb943b48bfd44f9f5a7d150193dab58e1
Etan Joseph Heyman <etan@heyman.net>
2023-03-17 15:42:19 -0500

    Fix height for responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 export default function Home() {
   return (
-    <div className={`flex h-full max-h-[62vh] w-full max-w-fit flex-col items-center justify-start gap-6 rounded-2xl bg-qrWhite p-4 text-center`}>
+    <div className={`flex min-h-[62vh] w-full max-w-fit flex-col items-center justify-start gap-6 rounded-2xl bg-qrWhite p-4 text-center`}>
       <Image className='rounded-lg' height={280} width={280} alt='qr code' src={'/image-qr-code.png'} />
       <div className='flex max-w-[260px] flex-col gap-4 pb-14'>
         <h2 className='text-xl font-bold tracking-wider text-qrDarkBlue'>Improve your front-end skills by building projects</h2>
